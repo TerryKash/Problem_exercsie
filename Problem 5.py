7d53ab7819ab88dbc52dab2bf026987a6074a57e
Tushar <tusharkashyap9945@gmail.com>
2020-06-24 15:03:40 +0530

    Add Problem 5

---
+++ Problem 5.py
@@ -0,0 +1,11 @@
+'''
+You are given a list which contains some numbers. You have to print a list of next palindromes only if the number is greater than 10 otherwise you will print that number.
+
+Input:
+
+[1, 6, 87, 43]
+
+Output:
+
+[1, 6, 88, 44
+'''
